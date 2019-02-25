3db10a4e5d3c4cb082f4f59568a17ec073fe99ea
pearcej <jan_pearce@berea.edu>
2019-02-25 09:02:22 -0500

    improved starter code

--- PDA/PDA-main.cpp
+++ PDA/PDA-main.cpp
@@ -7,12 +7,16 @@ using namespace std;
 void equal_10s() {
 	char readchar;
 
-	cout << "Please enter a binary string for testing 0^n 1^n: ";
+	cout << "Please enter a binary string for testing 0^n 1^n (enter 'q' to end): " <<endl;
 	do {
-		cin.get(readchar);
-		cout << readchar;
-	} while (readchar != '\n' && readchar != '\r' && readchar != '\0');
+		cin >>readchar;
+
+		//FIXME: Put code here
+
+	} while (readchar == '0' || readchar == '1');
+	
 	//FIXME: Put code here
+
 	cout << endl;
 	return;
 }
@@ -20,25 +24,30 @@ void equal_10s() {
 void equal_010s() {
 	char readchar;
 
-	cout << "Please enter a binary string for testing 0^n 1^m 0^n: ";
+	cout << "Please enter a binary string for testing 0^n 1^m 0^n: (enter 'q' to end):" << endl;
 	do {
-		cin.get(readchar);
-		cout << readchar;
-	} while (readchar != '\n' && readchar != '\r' && readchar != '\0');
+		cin >> readchar;
+
+		//FIXME: Put code here
+
+	} while (readchar == '0' || readchar == '1');
+
 	//FIXME: Put code here
+
 	cout << endl;
 	return;
 }
 
-int main( ){
+int main() {
 
 	char readchar;
 
 	equal_10s();
 	equal_010s();
 
+	cout << "FIXME: put an ending statment here." << endl;
+
 	cin >> readchar; //This is to keep screen open in some situations.
 
 	return 0;
 }
-
